2d253b80ad047732acc53bb28640326ed753e29e
GlebNikolenko <honorda@mail.ru>
2024-02-13 09:46:35 +0100

    Добавил логи

--- src/test/java/pages/EditCasePage.java
+++ src/test/java/pages/EditCasePage.java
@@ -3,11 +3,14 @@ package pages;
 import com.codeborne.selenide.Condition;
 import dto.TestCase;
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import wrappers.DropDown;
 import wrappers.ProseMirror;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
+
+@Log4j2
 
 public class EditCasePage {
     public final String EDIT_CASE_PAGE_HEADER = "//h1[text()='Edit test case']";
@@ -18,14 +21,17 @@ public class EditCasePage {
         $x(EDIT_CASE_PAGE_HEADER).shouldBe(Condition.visible);
         return this;
     }
+
     @Step("Editing the '{editedCase.title}' test case")
     public EditCasePage editTestCase(TestCase editedCase) {
+        log.info("The '{}' test case is edited", editedCase.getTitle());
         new ProseMirror("Description").writeCase(editedCase.getDescription());
         new DropDown("Severity").select(editedCase.getSeverity());
         new DropDown("Priority").select(editedCase.getPriority());
         new ProseMirror("Pre-conditions").writeCase(editedCase.getPreconditions());
         return this;
     }
+
     @Step("Clicking the save edited case button")
     public RepositoryPage clickOnSaveEditedCaseButton() {
         $(SAVE_EDITED_CASE_BUTTON).shouldBe(Condition.appear).click();
--- src/test/java/pages/TestCasePage.java
+++ src/test/java/pages/TestCasePage.java
@@ -3,24 +3,29 @@ package pages;
 import com.codeborne.selenide.Condition;
 import dto.TestCase;
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import wrappers.DropDown;
 import wrappers.ProseMirror;
 
 import static com.codeborne.selenide.Selenide.$;
 
+@Log4j2
 public class TestCasePage extends BasePage {
 
     public final String CASE_TITLE = "#title";
     public final String SAVE_BUTTON = "#save-case";
+
     @Step("Opening the 'Test case' page")
     public TestCasePage isPageOpened() {
         $(SAVE_BUTTON).shouldBe(Condition.visible);
         return this;
     }
+
     @Step("Creating a new test case")
     public TestCasePage createNewTestCase(TestCase testCase) {
         $(CASE_TITLE).clear();
         $(CASE_TITLE).setValue(testCase.getTitle());
+        log.info("The '{}' test case is created", testCase.getTitle());
         new DropDown("Status").select(testCase.getStatus());
         new ProseMirror("Description").writeCase(testCase.getDescription());
         new DropDown("Severity").select(testCase.getSeverity());
@@ -34,6 +39,7 @@ public class TestCasePage extends BasePage {
         new ProseMirror("Post-conditions").writeCase(testCase.getPostconditions());
         return this;
     }
+
     @Step("Clicking on the save button ")
     public RepositoryPage clickOnSaveNewCaseButton() {
         $(SAVE_BUTTON).shouldBe(Condition.appear).click();
